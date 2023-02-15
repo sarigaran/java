209db849df5c894c42f503213ee26592bf49d165
sarigaran <arigaran.sundaram@gmail.com>
2023-02-15 23:20:42 +0530

    multithreading

---
+++ Multithreading/src/multi/java/run.java
@@ -0,0 +1,28 @@
+package multi.java;
+
+class MultithreadingDemo implements Runnable {
+    public void run()
+    {
+        try {
+            // Displaying the thread that is running
+            System.out.println(
+                "Thread " + Thread.currentThread().getId()
+                + " is running");
+        }
+        catch (Exception e) {
+            // Throwing an exception
+            System.out.println("Exception is caught");
+        }
+    }
+}
+public class run {
+	 public static void main(String[] args)
+	    {
+	        int n = 8; // Number of threads
+	        for (int i = 0; i < n; i++) {
+	            Thread object
+	                = new Thread(new MultithreadingDemo());
+	            object.start();
+	        }
+	    }
+}
